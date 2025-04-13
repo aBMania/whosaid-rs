d970dc43ff2b99af5a816feeaba4df15d4effd16
Sylvain Bossut <sylvain.bossut@neofacto.com>
2025-04-13 23:16:49 +0200

    👷 add cargo build & test in ci

--- whosaid/src/database/mod.rs
+++ whosaid/src/database/mod.rs
@@ -1,3 +1,4 @@
+use std::env;
 use std::time::Duration;
 
 use anyhow::Result;
@@ -18,19 +19,8 @@ pub struct Database {
 
 impl Database {
     pub async fn new() -> Result<Self> {
-        let db_url = "postgres://postgres:whosaidp@localhost:15432/whosaid";
 
-        let mut opt = ConnectOptions::new(db_url);
-
-        opt.max_connections(100)
-            .min_connections(5)
-            .connect_timeout(Duration::from_secs(8))
-            .acquire_timeout(Duration::from_secs(8))
-            .idle_timeout(Duration::from_secs(8))
-            .max_lifetime(Duration::from_secs(8))
-            .sqlx_logging(true)
-            .sqlx_logging_level(log::LevelFilter::Info)
-            .set_schema_search_path("whosaid"); // Setting default PostgreSQL schema
+        let db_url = env::var("DATABASE_URL")?;
 
         let db = SeaOrmDatabase::connect(db_url).await?;
 
